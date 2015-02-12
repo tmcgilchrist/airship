ab8bd65da1a053f381374033802fc2c5b6112ed2
Reid Draper <reiddraper@gmail.com>
2015-02-12 16:02:01 -0600

    Wrap the whole decision tree in the state monad

--- src/Airship/Decision.hs
+++ src/Airship/Decision.hs
@@ -5,31 +5,42 @@ module Airship.Decision
     ( flow
     ) where
 
-import           Airship.Types (Handler, Response(..), halt, request)
-import           Airship.Resource(Resource(..))
+import           Airship.Types (Webmachine, Response(..), ResponseBody,
+                                halt, request)
+import           Airship.Resource(Resource(..), ContentType)
+import           Control.Monad.Trans (lift)
+import           Control.Monad.Trans.State.Strict (StateT(..), evalStateT,
+                                                   modify)
 import qualified Data.CaseInsensitive as CI
 import           Data.Maybe (fromJust)
 import qualified Network.HTTP.Types as HTTP
 import           Network.Wai (requestMethod, requestHeaders)
 
-type Flow s m = Resource s m -> Handler s m (Response m)
+data FlowState m = FlowState
+    { _contentType :: Maybe (ContentType, ResponseBody m)
+    }
 
-flow :: Flow s m
-flow = b13
+type Flow s m = Resource s m -> StateT (FlowState m) (Webmachine s m) (Response m)
 
-b13, b12, b11, b10, b09, b08, b07, b06, b05, b04, b03 :: Flow s m
-c04, c03 :: Flow s m
-d05, d04 :: Flow s m
+initFlowState :: FlowState m
+initFlowState = FlowState Nothing
+
+flow :: Monad m => Resource s m -> Webmachine s m (Response m)
+flow r = evalStateT (b13 r) initFlowState
+
+b13, b12, b11, b10, b09, b08, b07, b06, b05, b04, b03 :: Monad m => Flow s m
+c04, c03 :: Monad m => Flow s m
+d05, d04 :: Monad m => Flow s m
 
 b13 r@Resource{..} = do
-    available <- serviceAvailable
+    available <- lift serviceAvailable
     if available
         then b12 r
-        else halt HTTP.status503
+        else lift $ halt HTTP.status503
 
 b12 r@Resource{..} = do
     -- known method
-    req <- request
+    req <- lift request
     let knownMethods = [ HTTP.methodGet
                        , HTTP.methodPost
                        , HTTP.methodHead
@@ -40,69 +51,69 @@ b12 r@Resource{..} = do
                        , HTTP.methodOptions
                        , HTTP.methodPatch
                        ]
-    if (requestMethod req) `elem` knownMethods
+    if requestMethod req `elem` knownMethods
         then b11 r
-        else halt HTTP.status501
+        else lift $ halt HTTP.status501
 
 b11 r@Resource{..} = do
-    long <- uriTooLong
+    long <- lift uriTooLong
     if long
-        then halt HTTP.status414
+        then lift $ halt HTTP.status414
         else b10 r
 
 b10 r@Resource{..} = do
-    req <- request
-    allowed <- allowedMethods
-    if (requestMethod req) `elem` allowed
+    req <- lift request
+    allowed <- lift allowedMethods
+    if requestMethod req `elem` allowed
         then b09 r
-        else halt HTTP.status405
+        else lift $ halt HTTP.status405
 
 b09 r@Resource{..} = do
-    malformed <- malformedRequest
+    malformed <- lift malformedRequest
     if malformed
-        then halt HTTP.status400
+        then lift $ halt HTTP.status400
         else b08 r
 
 b08 r@Resource{..} = do
-    authorized <- isAuthorized
+    authorized <- lift isAuthorized
     if authorized
         then b07 r
-        else halt HTTP.status401
+        else lift $ halt HTTP.status401
 
 b07 r@Resource{..} = do
-    forbid <- forbidden
+    forbid <- lift forbidden
     if forbid
-        then halt HTTP.status403
+        then lift $ halt HTTP.status403
         else b06 r
 
 b06 r@Resource{..} = do
-    validC <- validContentHeaders
+    validC <- lift validContentHeaders
     if validC
         then b05 r
-        else halt HTTP.status501
+        else lift $ halt HTTP.status501
 
 b05 r@Resource{..} = do
-    known <- knownContentType
+    known <- lift knownContentType
     if known
         then b04 r
-        else halt HTTP.status415
+        else lift $ halt HTTP.status415
 
 b04 r@Resource{..} = do
-    large <- entityTooLarge
+    large <- lift entityTooLarge
     if large
-        then halt HTTP.status413
+        then lift $ halt HTTP.status413
         else b03 r
 
 b03 r@Resource{..} = do
-    req <- request
-    if (requestMethod req) == HTTP.methodOptions
-        then halt HTTP.status200
+    req <- lift request
+    if requestMethod req == HTTP.methodOptions
+        then lift $ halt HTTP.status200
         else c03 r
 
 c04 r@Resource{..} = do
     -- TODO: do proper content-type negotiation
-    req <- request
-    provided <- contentTypesProvided
+    req <- lift request
+    provided <- lift contentTypesProvided
 
     let types = fmap fst provided
         reqHeaders = requestHeaders req
@@ -110,11 +121,14 @@ c04 r@Resource{..} = do
         -- if an Accept header is present
         requestedType = CI.mk (fromJust (lookup HTTP.hAccept reqHeaders))
     if requestedType `elem` types
-        then d04 r
-        else halt HTTP.status406
+        then do
+            modify (\fs -> fs { _contentType =
+                                    Just (requestedType, undefined) })
+            d04 r
+        else lift $ halt HTTP.status406
 
 c03 r@Resource{..} = do
-    req <- request
+    req <- lift request
     let reqHeaders = requestHeaders req
     case lookup HTTP.hAccept reqHeaders of
         (Just _h) ->
@@ -122,5 +136,19 @@ c03 r@Resource{..} = do
         Nothing ->
             d04 r
 
-d04 = undefined
-d05 = undefined
+d04 r@Resource{..} = do
+    req <- lift request
+    let reqHeaders = requestHeaders req
+    case lookup HTTP.hAcceptLanguage reqHeaders of
+        (Just _h) ->
+            d05 r
+        Nothing ->
+            -- TODO
+            -- e05 r
+            undefined
+
+d05 _r@Resource{..} = do
+    langAvailable <- lift languageAvailable
+    if langAvailable
+        then undefined -- e05
+        else lift $ halt HTTP.status406
--- src/Airship/Resource.hs
+++ src/Airship/Resource.hs
@@ -6,6 +6,7 @@
 
 module Airship.Resource
     ( Resource(..)
+    , ContentType
     , serverError
     , defaultResource
     , singletonContentType
@@ -24,27 +25,28 @@ import Network.HTTP.Types
 type ContentType = CI ByteString
 
 data Resource s m =
-    Resource { allowedMethods           :: Handler s m [Method]
-             , serviceAvailable         :: Handler s m Bool
-             , implemented              :: Handler s m Bool
-             , resourceExists           :: Handler s m Bool
-             , isAuthorized             :: Handler s m Bool
-             , forbidden                :: Handler s m Bool
-             , allowMissingPost         :: Handler s m Bool
-             , malformedRequest         :: Handler s m Bool
-             , uriTooLong               :: Handler s m Bool
-             , knownContentType         :: Handler s m Bool
-             , validContentHeaders      :: Handler s m Bool
-             , entityTooLarge           :: Handler s m Bool
-             , deleteResource           :: Handler s m Bool
-             , deleteCompleted          :: Handler s m Bool
-             , postIsCreate             :: Handler s m Bool
-             , createPath               :: Handler s m (Maybe Text)
-             , processPost              :: Handler s m Bool
+    Resource { allowMissingPost         :: Handler s m Bool
+             , allowedMethods           :: Handler s m [Method]
              -- can't for the life of me figure out why the inner 'Webmachine'
              -- can't be a 'Handler', this it is probably something to do with
              -- our (maybe?) inappropriate use of type aliases for constraints?
              , contentTypesProvided     :: Handler s m [(ContentType, ResponseBody m)]
+             , createPath               :: Handler s m (Maybe Text)
+             , deleteCompleted          :: Handler s m Bool
+             , deleteResource           :: Handler s m Bool
+             , entityTooLarge           :: Handler s m Bool
+             , forbidden                :: Handler s m Bool
+             , implemented              :: Handler s m Bool
+             , isAuthorized             :: Handler s m Bool
+             , knownContentType         :: Handler s m Bool
+             , languageAvailable        :: Handler s m Bool
+             , malformedRequest         :: Handler s m Bool
+             , postIsCreate             :: Handler s m Bool
+             , processPost              :: Handler s m Bool
+             , resourceExists           :: Handler s m Bool
+             , serviceAvailable         :: Handler s m Bool
+             , uriTooLong               :: Handler s m Bool
+             , validContentHeaders      :: Handler s m Bool
              }
 
 
@@ -52,25 +54,25 @@ serverError :: Handler m s a
 serverError = finishWith (Response status500 [] Empty)
 
 defaultResource :: Resource s m
-defaultResource = Resource { allowedMethods         = return [methodGet]
-                           , serviceAvailable       = return True
+defaultResource = Resource { allowMissingPost       = return False
+                           , allowedMethods         = return [methodGet]
+                           , contentTypesProvided   = return [("text/html", helloWorld)]
+                           , createPath             = return Nothing
+                           , deleteCompleted        = return False
+                           , deleteResource         = return False
+                           , entityTooLarge         = return False
+                           , forbidden              = return False
                            , implemented            = return True
                            , isAuthorized           = return True
-                           , resourceExists         = return True
-                           , forbidden              = return False
-                           , allowMissingPost       = return False
-                           , malformedRequest       = return False
-                           , uriTooLong             = return False
                            , knownContentType       = return True
-                           , validContentHeaders    = return True
-                           , entityTooLarge         = return False
-                           , deleteResource         = return False
-                           , deleteCompleted        = return False
+                           , languageAvailable      = return True
+                           , malformedRequest       = return False
                            , postIsCreate           = return False
-                           , createPath             = return Nothing
                            , processPost            = return False
-                           -- should this be a map?
-                           , contentTypesProvided   = return [("text/html", helloWorld)]
+                           , resourceExists         = return True
+                           , serviceAvailable       = return True
+                           , uriTooLong             = return False
+                           , validContentHeaders    = return True
                            }
 
 helloWorld :: ResponseBody m

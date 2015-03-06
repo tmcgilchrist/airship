e11f73f68e010e775a24705511f44aca6bcee0c3
Reid Draper <reiddraper@gmail.com>
2015-03-06 11:52:36 -0600

    Add POST example to bin/Main

--- bin/Main.hs
+++ bin/Main.hs
@@ -1,11 +1,22 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
 
 module Main where
 
 import           Airship (resourceToWai)
-import           Airship.Resource (Resource(..), defaultResource, singletonContentType)
+import           Airship.Resource ( Resource(..)
+                                  , PostResponse(..)
+                                  , defaultResource
+                                  , singletonContentType
+                                  )
 import           Airship.Route (RoutingSpec, (#>), (</>), var, root)
-import           Airship.Types (ResponseBody(..), request, getState, params)
+import           Airship.Types ( ResponseBody(..)
+                               , Handler
+                               , request
+                               , getState
+                               , params
+                               )
 
 import           Blaze.ByteString.Builder.Html.Utf8 (fromHtmlEscapedText)
 
@@ -15,6 +26,7 @@ import           Control.Monad.Trans (liftIO)
 
 import           Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HM
+import qualified Data.ByteString.Lazy as LB
 import           Data.ByteString.Lazy.Char8 (unpack)
 import           Data.Maybe (fromMaybe)
 import           Data.Monoid ((<>))
@@ -23,6 +35,23 @@ import           Data.Text(Text, pack)
 import qualified Network.HTTP.Types as HTTP
 import           Network.Wai (strictRequestBody, requestHeaders)
 import           Network.Wai.Handler.Warp (run)
+
+-- ***************************************************************************
+-- Helpers
+-- ***************************************************************************
+
+getBody :: Handler s IO LB.ByteString
+getBody = do
+    req <- request
+    liftIO (strictRequestBody req)
+
+readBody :: Handler s IO Integer
+readBody = read . unpack <$> getBody
+
+routingParam :: Text -> Handler s m Text
+routingParam t = do
+    p <- params
+    return (p HM.! t)
 
 newtype State = State { _getState :: MVar (HashMap Text Integer) }
 
@@ -35,6 +64,7 @@ accountResource :: Resource State IO
 accountResource = defaultResource
     { allowedMethods = return [ HTTP.methodGet
                               , HTTP.methodHead
+                              , HTTP.methodPost
                               , HTTP.methodPut
                               ]
     , knownContentType = do
@@ -50,17 +80,35 @@ accountResource = defaultResource
         let val = fromMaybe 0 (accountNameM >>= flip HM.lookup m)
         return [("text/plain", ResponseBuilder (fromHtmlEscapedText (pack (show val) <> "\n")))]
 
-    , contentTypesAccepted = return [("text/plain", do
-        req <- request
+    , allowMissingPost = return False
+
+    , resourceExists = do
+        accountName <- routingParam "name"
         s <- getState
-        body <- liftIO (strictRequestBody req)
-        p <- params
-        let accountName = p HM.! "name"
-            val = read (unpack body)
+        m <- liftIO (readMVar (_getState s))
+        return $ HM.member accountName m
+
+    -- POST'ing to this resource adds the integer to the current value
+    , processPost = return (PostProcess $ do
+        (val, accountName, s) <- postPutStates
+        liftIO (modifyMVar_ (_getState s) (return . HM.insertWith (+) accountName val))
+        return ()
+        )
+
+    , contentTypesAccepted = return [("text/plain", do
+        (val, accountName, s) <- postPutStates
         liftIO (modifyMVar_ (_getState s) (return . HM.insert accountName val))
         return ()
     )]
     }
+
+postPutStates :: Handler State IO (Integer, Text, State)
+postPutStates = do
+    val <- readBody
+    accountName <- routingParam "name"
+    s <- getState
+    return (val, accountName, s)
+
 
 myRoutes :: RoutingSpec State IO ()
 myRoutes = do

8d01fc25c9e9a2edea07486a18ac47205e34b33a
Reid Draper <reiddraper@gmail.com>
2015-07-27 16:17:16 -0500

    Add HTTP redirect helper functions

--- src/Airship.hs
+++ src/Airship.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+
 module Airship
   ( module Airship.Resource
   , module Airship.Headers
   , module Airship.Helpers
   , module Airship.Route
   , module Airship.Types
+  , redirectTemporarily
+  , redirectPermanently
   ) where
 
 import           Airship.Headers
@@ -11,3 +16,17 @@ import           Airship.Helpers
 import           Airship.Resource
 import           Airship.Route
 import           Airship.Types
+
+import qualified Network.HTTP.Types as HTTP
+import           Data.ByteString (ByteString)
+
+-- | Issue an HTTP 302 (Found) response, with `location' as the destination.
+redirectTemporarily :: ByteString -> Handler s m a
+redirectTemporarily location =
+    addResponseHeader ("Location", location) >> halt HTTP.status302
+
+-- | Issue an HTTP 301 (Moved Permantently) response,
+-- with `location' as the destination.
+redirectPermanently :: ByteString -> Handler s m a
+redirectPermanently location =
+    addResponseHeader ("Location", location) >> halt HTTP.status301
